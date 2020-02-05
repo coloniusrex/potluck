c8364cba8393e187be246179ec1fa51fc95b1b0c
Colin Alexander <colintalex@gmail.com>
2020-02-04 22:48:45 -0700

    Adds test method for get_all_from_category.

--- test/potluck_test.rb
+++ test/potluck_test.rb
@@ -6,7 +6,10 @@ class PotluckTest < Minitest::Test
   def setup
     @potluck = Potluck.new("7-13-18")
     @couscous_salad = Dish.new("Couscous Salad", :appetizer)
+    @summer_pizza = Dish.new("Summer Pizza", :appetizer)
+    @roast_pork = Dish.new("Roast Pork", :entre)
     @cocktail_meatballs = Dish.new("Cocktail Meatballs", :entre)
+    @candy_salad = Dish.new("Candy Salad", :dessert)
   end
   def test_it_exists
     assert_instance_of Potluck, @potluck
@@ -26,5 +29,17 @@ class PotluckTest < Minitest::Test
     expected = [@couscous_salad, @cocktail_meatballs]
     assert_equal expected, @potluck.dishes
     assert_equal 2, @potluck.dishes.length
+  end
+
+  def test_get_all_dishes_by_category
+    @potluck.add_dish(@couscous_salad)
+    @potluck.add_dish(@summer_pizza)
+    @potluck.add_dish(@roast_pork)
+    @potluck.add_dish(@cocktail_meatballs)
+    @potluck.add_dish(@candy_salad)
+    expected = [@couscous_salad, @summer_pizza]
+    assert_equal expected, @potluck.get_all_from_category(:appetizer)
+    assert_equal @couscous_salad,
+    @potluck.get_all_from_category(:appetizer).first
   end
 end
